1f85358e0a56c0adcc5fcd80f1ada25a5beb3377
rodgerswisdom <trulyhawona@gmail.com>
2024-03-18 23:15:50 +0300

    max_value

---
+++ linked_list.py
@@ -0,0 +1,7 @@
+class linked_list:
+    def __init__(self, head):
+        self.head = head
+        self.next = None
+
+    def append:
+        
--- max_value.py
+++ max_value.py
@@ -4,7 +4,9 @@ def find_max(List):
     for item in List[1:]:
         if item > max_value:
             max_value = item
-    print(max_value)
-    # return max_value
+            print(max_value)
+    return max_value
+    
+    
 
     find_max([3,4,9,2,1])
