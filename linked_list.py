4eb3e027814d1195f5e2e3b38c735e0f740b68b1
rodgerswisdom <trulyhawona@gmail.com>
2024-03-19 10:18:21 +0300

    .

--- linked_list.py
+++ linked_list.py
@@ -1,15 +1,18 @@
 class linked_list:
     def __init__(self, head = None):
-        self.head = head
-        
+        self.head = head #inintializing the first node with only the head
+    
 
-        def append(self, new_node):
-            current = self.head
+        def append(self, new_node): # adding a new node at the end of the existing node
+            current = self.head # make the head the current node 
 
-            if current:
-                while current.next:
+            if current: 
+                while current.next: # if there is a next node, then make the next node the current, having iterated the list
                     current = current.next
-                curent.next = new_node
+                current.next = new_node # if there isnt, then make the next a new node
 
             else:
-                self.head = new_node
+                self.head = new_node # if non head, then maje the head a new node.and
+
+        def delete():
+            
